d90e3cc941e78d468fe507c04124feb9932dfa38
Programmer <shakirov.ekb@gmail.com>
2024-11-17 15:34:24 +0500

    Теперь каждое изображение в отдельном окне. Добавлены спинбоксы для установки размера изображения.

---
+++ Урок_26_Денис_05_Откр._каждую_карт._в_окне.py
@@ -0,0 +1,70 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox as mb
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+
+def get_dog_image():
+    try:
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data = response.json()
+        return data["message"]
+    except Exception as e:
+        mb.showerror("Ошибка", f"Возникла ошибка при запросе к API {e}")
+        return None
+
+
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img_size = (int(width_spinbox.get()), int(height_spinbox.get()))
+            img.thumbnail(img_size)
+            img = ImageTk.PhotoImage(img)
+            new_window = Toplevel(window)
+            new_window.title("Случайное изображение")
+            lb = ttk.Label(new_window, image=img)
+            lb.pack()
+            lb.image = img
+        except Exception as e:
+            mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения {e}")
+    progress.stop()
+
+
+def prog():
+    progress['value'] = 0
+    progress.start(30)
+    window.after(3000, show_image)
+
+
+window = Tk()
+window.title("Картинки с собачками")
+window.geometry("360x420")
+
+label = ttk.Label()
+label.pack(pady=10)
+
+button = ttk.Button(text="Загрузить изображение", command=prog)
+button.pack(pady=10)
+
+progress = ttk.Progressbar(mode="determinate", length=300)
+progress.pack(pady=10)
+
+width_label = ttk.Label(text="Ширина:")
+width_label.pack(side="left", padx=(0, 10))
+width_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+width_spinbox.pack(side="left", padx=(0, 10))
+
+height_label = ttk.Label(text="Высота:")
+height_label.pack(side="left", padx=(10, 0))
+height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+height_spinbox.pack(side="left", padx=(0, 10))
+
+window.mainloop()
